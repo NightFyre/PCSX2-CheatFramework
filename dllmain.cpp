89e9bb5f1a360dc72a89dee9ec039b64945584e3
NightFyre <80198020+xCENTx@users.noreply.github.com>
2023-08-12 15:16:07 -0400

    readme

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,10 +12,10 @@ DWORD WINAPI Client(LPVOID hInstance)
     MessageBoxA(NULL, "Initializing PCSX2 FrameWork", "DEBUG BUILD", MB_OK);
 #endif    
 
-    g_Running = PlayStation2::InitSDK();        //  Initialize PS2 Mini SDK
-    if (g_Running)
+    g_Running = PlayStation2::InitSDK();                                        //  Initialize PS2 Mini SDK
+    if (g_Running)                                                              //  Check the state of initialization
     {
-        PlayStation2::g_Engine->CreateConsole(" PCSX2 Framework Console ");     //  Manually spawn a console instance
+        PlayStation2::g_Engine->CreateConsole(" PCSX2 Framework Console ");     //  Manually spawn a console instance ( this will be an sdk initialization option at some point in the future )
         auto device         = *PlayStation2::CGlobals::g_gs_device;             //  Obtains the current render api device instance [@TODO: create method of obtaining current api class instance (GSDevice11)]
         auto renderer       = *PlayStation2::CGlobals::g_gs_renderer;           //  obtains Hardware renderer instance  
         auto emu_thread     = *PlayStation2::CGlobals::g_emu_thread;            //  obtains main emu thread instance
@@ -30,15 +30,19 @@ DWORD WINAPI Client(LPVOID hInstance)
         //  Render Loop
         do
         {
+            //  Exit Key
             if (GetAsyncKeyState(VK_END) & 1)
-                g_Running ^= 1;   //  Exit
+                g_Running ^= 1;   //  flips the bit
 
         } while (g_Running);
         
         //  cleanup
-        PlayStation2::g_Engine->DestroyConsole();
-        PlayStation2::ShutdownSDK();
+        if (PlayStation2::g_Engine->isConsolePresent())     //  check if console is present ( this will be included in SDK initialization later on )
+            PlayStation2::g_Engine->DestroyConsole();       //  Destroy console instance ( otherwise it would linger and all references to it would be lost without manually obtaining the instance )
+        PlayStation2::ShutdownSDK();                        //  Shutdown the sdk
     }
+
+    //  Exit
     FreeLibraryAndExitThread((HMODULE)hInstance, EXIT_SUCCESS);
     return EXIT_SUCCESS;
 }
@@ -46,10 +50,10 @@ DWORD WINAPI Client(LPVOID hInstance)
 //---------------------------------------------------------------------------------------------------
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwCallReason, LPVOID lpReserved)
 {
-    switch (dwCallReason) 
-    {
-        case (DLL_PROCESS_ATTACH):      CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Client, hModule, NULL, NULL);    break;
-        case (DLL_PROCESS_DETACH):      g_Running ^= 1;                                                                             break;
-    }
+    UNREFERENCED_PARAMETER(lpReserved);
+
+    if (dwCallReason == DLL_PROCESS_ATTACH)
+        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Client, hModule, NULL, NULL); 
+    
     return TRUE;
 }
