883f3e8c38ca9fb41c4e50c713b7a942f58c542f
NightFyre <80198020+xCENTx@users.noreply.github.com>
2023-08-31 11:58:48 -0400

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -12,13 +12,13 @@ DWORD WINAPI Client(LPVOID hInstance)
     MessageBoxA(NULL, "Initializing PCSX2 FrameWork", "DEBUG BUILD", MB_OK);
 #endif    
 
-    g_Running = PlayStation2::InitSDK();                                        //  Initialize PS2 Mini SDK
-    if (g_Running)                                                              //  Check the state of initialization
+    g_Running = PlayStation2::InitSDK();                                                //  Initialize PS2 Mini SDK
+    if (g_Running)                                                                      //  Check the state of initialization
     {
-        PlayStation2::g_Engine->CreateConsole(" PCSX2 Framework Console ");     //  Manually spawn a console instance ( this will be an sdk initialization option at some point in the future )
-        auto device         = *PlayStation2::CGlobals::g_gs_device;             //  Obtains the current render api device instance [@TODO: create method of obtaining current api class instance (GSDevice11)]
-        auto renderer       = *PlayStation2::CGlobals::g_gs_renderer;           //  obtains Hardware renderer instance  
-        auto emu_thread     = *PlayStation2::CGlobals::g_emu_thread;            //  obtains main emu thread instance
+        PlayStation2::g_Engine->CreateConsole(" PCSX2 Cheat Framework Console ");       //  Manually spawn a console instance ( this will be an sdk initialization option at some point in the future )
+        auto device         = *PlayStation2::CGlobals::g_gs_device;                     //  Obtains the current render api device instance [@TODO: create method of obtaining current api class instance (GSDevice11)]
+        auto renderer       = *PlayStation2::CGlobals::g_gs_renderer;                   //  obtains Hardware renderer instance  
+        auto emu_thread     = *PlayStation2::CGlobals::g_emu_thread;                    //  obtains main emu thread instance
         
         printf("[+] PCSX2 Framework Client Initialized!\n- device\t-> 0x%llX\n- renderer\t-> 0x%llX\n- thread\t-> 0x%llX\n- PS2Base\t-> 0x%llX\n"
             ". . . PRESS [END] TO EXIT . . .\n\n",
@@ -47,12 +47,9 @@ DWORD WINAPI Client(LPVOID hInstance)
 }
 
 //---------------------------------------------------------------------------------------------------
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwCallReason, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  dwCallReason, LPVOID)
 {
-    UNREFERENCED_PARAMETER(lpReserved);
-
     if (dwCallReason == DLL_PROCESS_ATTACH)
-        CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Client, hModule, NULL, NULL); 
-    
+        CloseHandle(CreateThread(0, 0, Client, hModule, 0, 0));
     return TRUE;
 }
