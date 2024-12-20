696cafc477f730fa59ff79565b4e77a6d6188530
NightFyre <80198020+xCENTx@users.noreply.github.com>
2024-12-19 21:20:46 -0500

    Basic Memory Class
    Obtains ProcID, Module base address and EEMemory
    - Read String method

---
+++ Memory.cpp
@@ -0,0 +1,160 @@
+#include "Memory.h"
+
+PCSX2Memory::PCSX2Memory() { bValidInstance = ResolveProcess("pcsx2-qt.exe", g_pcsx); }
+
+PCSX2Memory::PCSX2Memory(const std::string& name) { bValidInstance = ResolveProcess(name, g_pcsx); }
+
+PCSX2Memory::~PCSX2Memory() {}
+
+bool PCSX2Memory::ResolveProcess(const std::string& pName, pcsx2_t& pInfo)
+{
+	pInfo = pcsx2_t();
+
+	std::wstring key = std::wstring(pName.begin(), pName.end());
+	std::transform(key.begin(), key.end(), key.begin(), [](unsigned char c) { return std::tolower(c); });
+	if (!key.size())
+		return false;
+
+	///	GET PROCESS ID
+	HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnap == INVALID_HANDLE_VALUE)
+		return false;
+
+	PROCESSENTRY32 pEntry;
+	pEntry.dwSize = sizeof(pEntry);
+	if (!Process32First(hSnap, &pEntry))
+	{
+		CloseHandle(hSnap);
+		return false;
+	}
+
+	bool bFound{ false };
+	do
+	{
+		auto name = std::wstring(pEntry.szExeFile);
+		std::transform(name.begin(), name.end(), name.begin(), [](unsigned char c) {return std::tolower(c); });
+		if (name != key)
+			continue;
+
+		bFound = true;
+		pInfo.dwProcID = pEntry.th32ProcessID;
+
+	} while (Process32Next(hSnap, &pEntry));
+	CloseHandle(hSnap);
+
+	if (!bFound || !pInfo.dwProcID)
+	{
+		pInfo = pcsx2_t();
+		return false;
+	}
+
+	///	GET PROCESS MODULE BASE
+	hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pInfo.dwProcID);
+	if (hSnap == INVALID_HANDLE_VALUE)
+	{
+		pInfo = pcsx2_t();
+		return false;
+	}
+
+	MODULEENTRY32 me32;
+	me32.dwSize = sizeof(me32);
+	if (!Module32First(hSnap, &me32))
+	{
+		pInfo = pcsx2_t();
+		CloseHandle(hSnap);
+		return false;
+	}
+	CloseHandle(hSnap);	//	first module is PCSX2
+
+	pInfo.uBaseAddress = (long long)me32.modBaseAddr;	//	module base address
+
+	///	OBTAIN HANDLE TO PROCESS
+	g_pcsx.hProc = OpenProcess(PROCESS_ALL_ACCESS, false, g_pcsx.dwProcID);
+	if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+	{
+		pInfo = pcsx2_t();
+		return false;
+	}
+
+	/// GET EEMem
+	auto dosHeader = Read<IMAGE_DOS_HEADER>(pInfo.uBaseAddress);
+	if (dosHeader.e_magic != IMAGE_DOS_SIGNATURE)
+	{
+		pInfo = pcsx2_t();
+		return false;
+	}
+
+	auto ntHeader = Read<IMAGE_NT_HEADERS>(pInfo.uBaseAddress + dosHeader.e_lfanew);
+	for (IMAGE_DATA_DIRECTORY& directory : ntHeader.OptionalHeader.DataDirectory)
+	{
+		__i64 exportRVA = pInfo.uBaseAddress + directory.VirtualAddress;
+		if (!exportRVA)
+			continue;
+
+		auto exports = Read<IMAGE_EXPORT_DIRECTORY>(exportRVA);
+		auto namesRVA = pInfo.uBaseAddress + exports.AddressOfNames;
+		auto functionsRVA = pInfo.uBaseAddress + exports.AddressOfFunctions;
+		for (int i = 0; i < exports.NumberOfNames; i++)
+		{
+			auto nameOffset = Read<__i32>(namesRVA + i * sizeof(DWORD));
+			auto fnOffset = Read<__i32>(functionsRVA + i * sizeof(DWORD));
+
+			std::string name;
+			if (!ReadString(pInfo.uBaseAddress + nameOffset, name))
+				continue;
+
+			if (name != "EEmem")
+				continue;
+
+			auto addr = Read<__i64>(pInfo.uBaseAddress + fnOffset);
+			if (addr)
+			{
+				pInfo.uEEmem = addr;
+				break;
+			}
+			else
+			{
+				// bad error this shouldnt happen ; game is not running
+				pInfo = pcsx2_t();
+				return false;
+			}
+		}
+
+		if (pInfo.uEEmem)
+			break;
+	}
+
+	return true;
+}
+
+__i64 PCSX2Memory::GetAddr(const __i32& offset)
+{
+	if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+		return false;
+
+	return g_pcsx.uEEmem + offset;
+}
+
+bool PCSX2Memory::ReadString(const __i64& addr, std::string& result)
+{
+	if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+		return false;
+
+	char buff[MAX_PATH];
+	if (!ReadProcessMemory(g_pcsx.hProc, (void*)addr, &buff, MAX_PATH, nullptr))
+		return false;
+
+	result = std::string(buff);	//	@TODO zero terminate? lol
+
+	return true;
+}
+
+bool PCSX2Memory::ReadPointerChain(const __i64& addr, const std::vector<__i32>& offsets, __i64& result)
+{
+	if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+		return false;
+
+	//	@TODO : 
+
+	return false;
+}
---
+++ Memory.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <Windows.h>
+#include <memoryapi.h>
+#include <TlHelp32.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+typedef unsigned __int8 __i8;		//	1Byte
+typedef unsigned __int16 __i16;		//	2Bytes
+typedef unsigned __int32 __i32;		//	4Bytes
+typedef unsigned __int64 __i64;		//	8Bytes
+
+struct PCSX2PROCESSINFO64
+{
+	HANDLE hProc{ INVALID_HANDLE_VALUE };		//	handle to process
+	DWORD dwProcID{ 0 };						//	process id
+	__i64 uBaseAddress{ 0 };					//	process module base address -> IMAGE_DOS_HEADER
+	__i64 uEEmem{ 0 };							//	pcsx2 game module base address
+}; typedef PCSX2PROCESSINFO64 pcsx2_t;
+inline pcsx2_t g_pcsx;	//	static pointer to process information
+
+//	External Memory Class
+class PCSX2Memory
+{
+private:
+	bool ResolveProcess(const std::string& pName, pcsx2_t& pInfo);
+
+public:
+	bool bValidInstance{ false };
+
+public:
+	//	returns eemem + offset
+	static __i64 GetAddr(const __i32& offset);
+
+	//	reads a continguous string in memory
+	static bool ReadString(const __i64& addr, std::string& result);
+
+	//	reads a chain of offsets and returns the result
+	static bool ReadPointerChain(const __i64& addr, const std::vector<__i32>& offsets, __i64& result);
+
+	//	template static methods
+public:
+	template<typename T>
+	T Read(const __i64& addr) noexcept
+	{
+		if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+			return T{};
+
+		T result{};
+		ReadProcessMemory(g_pcsx.hProc, (void*)addr, &result, sizeof(T), nullptr);
+		return result;
+	}
+
+	template<typename T>
+	bool Write(const __i64& addr, const T& value)
+	{
+		if (g_pcsx.hProc == INVALID_HANDLE_VALUE)
+			return false;
+
+		SIZE_T bytesWritten;
+		return WriteProcessMemory(g_pcsx.hProc, (void*)addr, &value, sizeof(T), &bytesWritten) && bytesWritten == sizeof(T);
+	}
+
+	//	constructors
+public:
+	explicit PCSX2Memory();
+	PCSX2Memory(const std::string& processName);
+	~PCSX2Memory();
+};
